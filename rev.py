0e0e1800d96e5b5c1aa6fd61f3f548bcca6bb222
Kazi Priom <109478175+ItsIzakB@users.noreply.github.com>
2024-11-20 19:35:21 -0600

    Update rev.py

--- rev.py
+++ rev.py
@@ -1,3 +1,5 @@
+
+
 #priting
 
 hello = 'tim'
@@ -7,4 +9,27 @@ world = 'world'
 hello = 'hello'
 
 print(hello, world)
+
+# input
+#
+# input =input()
+#
+# print (input)
+
+#typecasting
+
+# integer = 12
+#
+# intToFloat = float(integer)
+
+word = 45
+
+wordToInt = int(word)
+
+word += 5
+
+print(word)
+
+print(type(word))
+
 
